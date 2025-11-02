88b6cb425669eb851109b800902f01e09c4d96cc
BidangPKP <nailahnafanil@gmail.com>
2025-11-02 10:31:06 +0700

    SIGAP KUMUH

---
+++ data/PekerjaanPermukiman2025_8.js
@@ -0,0 +1,8 @@
+var json_PekerjaanPermukiman2025_8 = {
+"type": "FeatureCollection",
+"name": "PekerjaanPermukiman2025_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No SPK": 0.0, "Y": -7.14302, "X": 112.39671, "Nama Kegia": "Peningkatan Jalan Lingkungan Wajik Kec. Lamongan", "Jenis": "Jalan", "Pekerjaan": "CBC", "Dana": "APBD-P", "Dusun": null, "Desa/Kelur": "Wajik", "Kecamatan": "Lamongan", "Progress": 0.0, "Sebelum": null, "Sesudah": null }, "geometry": { "type": "Point", "coordinates": [ 112.396707, -7.143019 ] } }
+]
+}
